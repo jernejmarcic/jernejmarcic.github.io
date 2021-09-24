a5e39781bca97217ca24d0ea571b2f8f882ab97f
jernejmarcic <jernej.marcicprotonmail.com>
2021-09-24 20:04:12 +0200

    website contact info looks nice

---
+++ index.js
@@ -0,0 +1,14 @@
+
+    function adialog() {
+
+    var img = document.createElement("img");
+    img.src = "https://1.bp.blogspot.com/-u9y9lRI05vI/W0-GEXVUpaI/AAAAAAAAK4M/PiEa3rkauLQxKP8NlJxchytT_gcJ3jvHQCLcBGAs/s1600/WindowsDefenederIconActionNeeded.png";
+    var ele = document.getElementById("body");
+    ele.appendChild(img);
+
+}
+
+    function modify_text() {
+    var ele = document.getElementById("text_ele");
+    ele.innerHTML = "DANKMEMES are GONE!!!!!!";
+}
